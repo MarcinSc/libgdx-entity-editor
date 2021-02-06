402c37de245f2f3d8e3d16944f5932a137952a80
marcin.sciesinski <marcin.sciesinski>
2021-02-06 02:02:00 -0800

    Initial commit

---
+++ core/src/main/java/com/gempukku/libgdx/entity/editor/GdxEntityEditor.java
@@ -0,0 +1,9 @@
+package com.gempukku.libgdx.entity.editor;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+/**
+ * {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms.
+ */
+public class GdxEntityEditor extends ApplicationAdapter {
+}
