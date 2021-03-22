f8329207ee8fd039deba3e3ce9739cab05c51931
marcin.sciesinski <marcin.sciesinski>
2021-03-22 15:19:46 -0700

    Fixing bug with empty properties

--- core/src/main/java/com/gempukku/libgdx/entity/editor/data/component/CustomDataStorage.java
+++ core/src/main/java/com/gempukku/libgdx/entity/editor/data/component/CustomDataStorage.java
@@ -38,6 +38,8 @@ public class CustomDataStorage implements DataStorage {
     }
 
     private JsonValue clone(JsonValue value) {
+        if (value == null)
+            return new JsonValue(JsonValue.ValueType.nullValue);
         if (value.isString())
             return new JsonValue(value.asString());
         JsonReader reader = new JsonReader();
